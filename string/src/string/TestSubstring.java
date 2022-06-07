344a8e8fb547edf66f06352f4fdbaad1e6d7ce7e
aashu <aashu@AASHUALI>
2022-06-07 16:22:41 +0530

    first commit

---
+++ string/src/string/TestSubstring.java
@@ -0,0 +1,10 @@
+package string;
+public class TestSubstring
+{
+	public static void main(String args[]) {
+		String s="hardik Pandya";
+		System.out.println(s.substring(7));
+		System.out.println(s.substring(0,7));
+		
+	}
+}
